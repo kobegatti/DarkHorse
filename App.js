f2b1398fcbdb8bcc5e413dd0ffd38cfad30ff993
Kobe Gatti <59642083+kobegatti@users.noreply.github.com>
2022-01-03 14:33:59 -0800

    first publish on expo

--- App.js
+++ App.js
@@ -3,18 +3,16 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  console.log("App executed");
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Text>Let's go DarkHorse</Text>
       <Text>More text is it updating?</Text>
       <Text>What's good people?</Text>
-      <Text>Is this a fake video?</Text>
-      <Text>No it ain't!</Text>
-      <Text>Now it's time for... IOS simulator(Hackintosh VirtualBox?)</Text>
-      <Text>On VD!</Text>
-      <Text>Shrimp pasta</Text>
       <Text>Testing Git!!!</Text>
+      <Text>By March, we will have a working mobile app</Text>
       <StatusBar style="auto" />
     </View>
   );
